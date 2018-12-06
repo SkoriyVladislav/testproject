8dc73c683bc864f6abd695b70c80b689c03305ff
julia-mur13 <juliamurashko158@gmail.com>
2018-12-06 13:24:11 +0300

    Add ui pack 1.0.7

--- ui/src/bundles/bundle.ts
+++ ui/src/bundles/bundle.ts
@@ -5,19 +5,21 @@ import '../components/core/slide-carousel/slide-carousel-dots';
 import '../components/core/dropdown-menu/dropdown-input';
 import '../components/core/dropdown-menu/dropdown-menu';
 import '../components/core/core-ts/lazyload'
+import '../components/core/core-ts/localization/label-i18n'
 
 import VideoService from '../components/core/video-overlay/video-service';
 import VideoTrigger from '../components/core/video-overlay/video-trigger';
 import VideoOverlay from "../components/main-page/video-overlay/video-overlay";
 
+import Localization from "../components/core/core-ts/localization/localization";
+
 
 
 const COMPONENTS_LIST = [
     VideoService,
-    VideoOverlay
+    VideoOverlay,
+    Localization
 ];
-
-
 
 COMPONENTS_LIST.forEach((Component) => {
     const component = new Component();
